d3effbb246de1e6c09cf0fa48ba38276da2fea59
riveramosqueda <riveramosquedajuan@gmail.com>
2022-07-27 01:23:41 -0500

    Added a few translations

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Su contraseña ha sido reestablecida!',
+    'sent' => 'Le enviamos por email su enlace para reestrablecer su contraseña!',
+    'throttled' => 'Por favor espere para intentar nuevamente.',
+    'token' => 'Este token de reestrablecimiento de contraseña es inválido.',
+    'user' => "No podemos encontrar el usuario con esa dirección email.",
+
+];
